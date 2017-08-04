6519145c2aa678295db800a9d04b642ffd84ff75
Alan-Miller <alan.ashman@gmail.com>
2017-08-04 03:16:32 -0600

    added routes etc

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
-import PokeForm from './PokeForm/PokeForm';
+import Arena from './Arena/Arena';
 import Header from './Header/Header';
+import Wordnik from './Wordnik/Wordnik';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
 
   constructor() {
     super()
     this.state = {}
-    this.evolve = this.evolve.bind(this);
-  }
-
-  evolve(stage) {
-    const imgClasses = document.getElementsByClassName('pokeImg')[0].classList;
-    const imgDivClasses = document.getElementsByClassName('pokeImgDiv')[0].classList;
-    imgClasses.add('hidden');
-    imgDivClasses.add('glow');
-
-    axios
-    .get(`http://pokeapi.co/api/v2/pokemon/${stage}`)
-    .then((response) => {
-      imgClasses.remove('hidden');
-      imgDivClasses.remove('glow');
-      this.setState({pokemon: {name: response.data.name, sprites: response.data.sprites}})
-    })
-  }
-
-  componentWillMount() {
-    axios
-    .get('http://pokeapi.co/api/v2/pokemon/1')
-    .then((response) => {
-      var pokeName = response.data.name
-      this.setState({pokemon: {name: pokeName, sprites: response.data.sprites}})
-    })
   }
 
   render() {
 
-    const poke = !this.state.pokemon ? <h2 style={ {color: "white"} }>Gotta catch 'em all...</h2> : (
-      <PokeForm evolve={this.evolve} name={this.state.pokemon.name} sprite={this.state.pokemon.sprites.front_default} />
-    )
     return (
-      <div className="App">
-        <Header />       
-        {poke}
-      </div>
-      
+        <div>
+             <Header /> 
+             <Switch> 
+                <Route component={Arena} exact path="/" />
+                <Route component={Wordnik} path="/wordnik" /> 
+             </Switch> 
+        </div>
     );
   }
 }
--- src/services/pokeService.js
+++ src/services/pokeService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export function getPokemon() {
-    return axios.get('http://pokeapi.co/api/v2/pokemon/5/').then((charmander) => charmander)
+export function getFirstPokemon() {
+    return axios.get(`http://pokeapi.co/api/v2/pokemon/1`).then(pokemon => pokemon.data);
 }
 
+export function getPokemon(stage) {
+    return axios.get(`http://pokeapi.co/api/v2/pokemon/${stage}`).then(pokemon => pokemon.data);
+}
