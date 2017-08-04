2116458a34b77f8bcb9b024661b893f3765de82f
Alan-Miller <alan.ashman@gmail.com>
2017-08-04 09:51:20 -0600

    edits

--- src/services/pokeService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function getFirstPokemon() {
-    return axios.get(`http://pokeapi.co/api/v2/pokemon/1`).then(pokemon => pokemon.data);
-}
-
-export function getPokemon(stage) {
-    return axios.get(`http://pokeapi.co/api/v2/pokemon/${stage}`).then(pokemon => pokemon.data);
-}
